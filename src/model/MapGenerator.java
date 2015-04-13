f18053f5b6b7fe12e4bbce86dcea07d20323820b
unknown <nrosati@FCM-204-01G-2.fcm.arizona.edu>
2015-04-13 09:23:01 -0700

    Trying to figure this Git stuff out

--- src/model/MapGenerator.java
+++ src/model/MapGenerator.java
@@ -1,9 +1,20 @@
 package model;
 
 public abstract class MapGenerator {
+	private int mapType;
 
 	public MapGenerator() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	private int generateMap(){
+		return mapType;
+		
+	}
+	
+	private void Draw(){
+		//Not going to be void
+		//Will do something in GUI
 	}
 
 }
