e803e7180ca38e0b0e223fc25888179515ce218f
Issak Sanchez <issaksanchez@email.arizona.edu>
2015-04-24 22:21:35 -0700

    GameWorld Updated -Issak/Michel
    Added more tests to the main method

--- src/model/GameWorld.java
+++ src/model/GameWorld.java
@@ -1,10 +1,12 @@
 package model;
 
+import javax.annotation.Resource;
+
 public class GameWorld {
 	//Map map = Map.getMap();
 	//int [][] field = map.getField();
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 		Map map = Map.getMap();
 		Map map2 = Map.getMap();
 		Tile [][] field = map.getField();
@@ -26,9 +28,10 @@ public class GameWorld {
 			}
 		}
 		int storage = agent.getStorage();
-		System.out.println("Agents resources = " + storage);
+		System.out.println("Agent's resources = " + storage);
 		agent.setAgent(0,0);
 		//boolean built = agent.buildBuilding(agent.getStorage(), 1, 1);
+		//builds building and returns storage
 		Building building = agent.buildBuilding(agent.getStorage(), 1, 1);
 		for(int i = 0; i < 100; i++){
 			for(int j = 0; j < 100; j++){
@@ -38,15 +41,32 @@ public class GameWorld {
 		}
 		//System.out.println("Did the building get built? " + built);
 		storage = agent.getStorage();
-		System.out.println("Agents resources after building = " + storage);
+		System.out.println("Agent's resources after building = " + storage);
 		
+		//informs that resource is being deposited
+		System.out.println("Depositing resources...........");
 		
-		
-		System.out.println("Depositing resources");
-		
-		agent.depositResources(1,building);
+		//deposits resources
+		agent.depositResources(1);
 		storage = agent.getStorage();
+		
 		System.out.println("Agents resources after deposit = " + storage);
+		
+		//next prove that agent can collect resources
+		
+		System.out.println("Agent will then collect food.");
+		ResourcesFood food = null;
+		agent.gatherResources(food);
+		
+		agent.getStorage();
+		
+		System.out.println("Agents resources after collecting resources = " + agent.getStorage());
+		
+		System.out.println("Next, tests what happens if an agent does not eat for too long.");
+		//tests what happens if an agent does not eat, they will become philosophers, which in this game world is no better than death
+		//also shows 1 fully implemented need, hunger
+		Agent dyingAgent = new AgentWarrior("Socrates", 1, 1);		
+		dyingAgent.slowlyDie();
+				
 	}
-
 }
