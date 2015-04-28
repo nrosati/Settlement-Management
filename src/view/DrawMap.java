9176d0405e33a2dfac6c46b14c80575e6faabc2e
esotelo1 <esotelo1@email.arizona.edu>
2015-04-28 16:13:11 -0700

    Revert "Revert "Started work on drawing the map.  Need the image files to see what may actually be going on.""
    This reverts commit c9003770f358316fb651806f36e26bb0c91c5da1.

---
+++ src/view/DrawMap.java
@@ -0,0 +1,44 @@
+package view;
+import java.awt.*;
+import model.*;
+
+import javax.swing.*;
+public class DrawMap extends JPanel{
+	private static  Map map = Map.getMap(); 
+	private static Tile[][] field = map.getField();
+	
+	private static final int NUM_ROWS = 20;
+	private static final int NUM_COLS = 20;
+	
+	public static final int PREFERRED_GRID_SIZE_PIXELS = 10;
+	public DrawMap() {
+		// TODO Auto-generated constructor stub
+		int prefereedWidth = NUM_COLS * PREFERRED_GRID_SIZE_PIXELS;
+		int preferredHiehgt = NUM_ROWS * PREFERRED_GRID_SIZE_PIXELS;
+		setPreferredSize(new Dimension(prefereedWidth, preferredHiehgt));
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.clearRect(0, 0, getWidth(), getHeight());
+		int w = getWidth() / NUM_COLS;
+		int h = getHeight() / NUM_ROWS;
+		for(int i = 0; i < NUM_ROWS; i++){
+			for(int j = 0; j < NUM_COLS; j++){
+				field[i][j].drawTile(g,w,h);//maybe g?
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		JFrame frame = new JFrame("GameMap");
+		DrawMap draw = new DrawMap();
+		frame.add(draw);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
