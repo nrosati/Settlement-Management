dea505a8d669f74d652f214d116528522b86705b
msanchezparamo <michelsanchez@email.arizona.edu>
2015-04-29 17:50:03 -0700

    drawtest
    test

---
+++ src/view/DrawTest.java
@@ -0,0 +1,76 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.image.ImageProducer;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.Map;
+import model.Tile;
+
+//Class prints out map in a JFrame
+//Problem: it takes a while for the map to propagate
+//hopefully we can hurry this up or make some kind of loading screen if we get lazy
+
+public class DrawTest {
+	
+			private Map map = Map.getMap(); 
+			private Tile[][] field = map.getField();	 
+	        private JFrame frame = new JFrame(); //creates frame	        
+	        private JLabel test;
+	        private JLabel test2;
+	        private ImageIcon icon = new ImageIcon("images/water.png");
+	        private Tile water;
+	        
+	        
+	        
+	 
+	        public DrawTest(){ 
+	         
+	        	 //  test2 = new JLabel(icon);
+	        	
+	               frame.setLayout(new GridLayout(100,100)); //set layout size
+	               frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       			   frame.setPreferredSize(new Dimension(1200, 1000));
+	      
+	                        // propagates map 75 x 75
+       			   			String mapTile = "";
+	                        for(int i = 0; i < 100; i++){
+	                			for(int j = 0; j < 100; j++){
+	                				int tile = field[i][j].getResourceType();
+	                				mapTile += tile;
+	                				
+	                			
+	                				test = new JLabel(mapTile + "");	    
+	                				
+	                				                				
+	          
+	                				test.setSize(100, 100);
+	                				frame.add(test);
+	          
+	                				//frame.add(test2);	                			            			
+	          
+		        	                frame.pack(); //sets appropriate size for frame
+		        	                frame.setVisible(true); //makes frame visible
+		                		
+	                			}
+	                	
+	                		frame.add(test);
+	                		         		
+	                }
+	                        
+	                        
+	                       
+	                
+	        }
+	        public static void main(String[] args) {
+	                new DrawTest();// propagates new map
+	        }
+	}
