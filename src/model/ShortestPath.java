3fd7e594ad6794f31398051ca4e426fe87c6a52d
esotelo1 <everett.sotelo@hermod.mural.local>
2015-05-05 08:06:49 -0700

    Revert "Revert "Timer Implemented and Walking""
    This reverts commit 2bb46a81ec343e398bb646dc5d9eaefbc6cffc18.

--- src/model/ShortestPath.java
+++ src/model/ShortestPath.java
@@ -3,6 +3,8 @@ package model;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ShortestPath {
 
@@ -17,6 +19,7 @@ public class ShortestPath {
 	private static Tile[][] field = map.getField();
 	private static Tile[][] walkPath;
 
+
 	LinkedList<Vertex> path;
 
 	public ShortestPath(int sourceX, int sourceY, int destX, int destY) {
@@ -30,33 +33,67 @@ public class ShortestPath {
 		Dijkstra dijkstra = new Dijkstra(graph);
 
 		dijkstra.execute(nodes.get(finalSource));
-		path = dijkstra.getPath(nodes.get(finalDest-1));
+		path = dijkstra.getPath(nodes.get(finalDest-2));
+
 		
-		int i;
-		int j;
-		String[] str = new String[1000];
+		Thread one = new Thread() {
+		    public void run() {
+			int i;
+			int j;
+			String[] str = new String[1000];
+		    for (Vertex vertex : path) {
+		    	
+		    	try {
+					Thread.sleep(250);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+		        System.out.println(vertex);
+		        String tempString = vertex.toString();
+		        str = tempString.split("[^\\d]+");
+		        i = Integer.parseInt(str[1]);
+		        j = Integer.parseInt(str[2]);
+		        
+		        //int temp = field[i][j].getResourceType();
+		        //field[i][j].setResourceType();
+		        //walk();
+		        field[i][j].setAgent();
+		        
+		        if(field[i-1][j].getResourceType() == 7 /*&& field[i-1][j].getAgent() == field[i][j].getAgent()*/)
+		        	//temp = field[i-1][j].getResourceType();
+		        	field[i-1][j].setResourceType(0);
+		        
+		        if(field[i][j-1].getResourceType() == 7 /*&& field[i][j-1].getAgent() == field[i][j].getAgent()*/) 
+		        	//temp = field[i][j-1].getResourceType();
+		        	field[i][j-1].setResourceType(0);
+		        
+		        if(field[i+1][j].getResourceType() == 7 /*&& field[i+1][j].getAgent() == field[i][j].getAgent()*/)
+		        	//temp = field[i+1][j].getResourceType();
+		        	field[i+1][j].setResourceType(0);
+		        
+		        if(field[i][j+1].getResourceType() == 7 /*&& field[i][j+1].getAgent() == field[i][j].getAgent()*/)
+		        	//temp = field[i][j+1].getResourceType();
+		        	field[i][j+1].setResourceType(0);
+
+		        
+		        //System.out.println(getPath().length);
+		        //walkPath[i][j].setResourceType(4);
+		        
+		       // System.out.println(walkPath[i][j]);
+		        
+		        //System.out.print(walkPath[i][j]);
+		     
+		        }
+		    //System.out.print(getPathInt());
+		    }
+		};
 		
-	    for (Vertex vertex : path) {
-	        System.out.println(vertex);
-	        String tempString = vertex.toString();
-	        str = tempString.split("[^\\d]+");
-	        i = Integer.parseInt(str[1]);
-	        j = Integer.parseInt(str[2]);
-	        
-	        //int temp = field[i][j].getResourceType();
-	        //field[i][j].setResourceType();
-	        //walk();
-	        field[i][j].setAgent();
-	        
-	        //System.out.println(getPath().length);
-	        walkPath[i][j].setResourceType(4);
-	        
-	       // System.out.println(walkPath[i][j]);
-	        
-	        //System.out.print(walkPath[i][j]);
-	     
-	        }
-	    System.out.print(getPathInt());
+		
+		one.start();
+
+
 	    }
 	    
 	   //for(int s = 0; s < str.length; s++) {
@@ -102,20 +139,20 @@ public class ShortestPath {
 		
 		for (int i = sourceX; i <= destX; i++) {
 			for (int j = sourceY; j <= destY; j++) {
-				if (i > 0 && (i - 1) <= 100 && field[i][j].getPassable()) {
+				if (i > 0 && (i - 1) <= 100 && field[i][j].getResourceType() != 2) {
 					addLane("edge_" + i + j, arrayAt[i][j], arrayAt[i - 1][j],
 							weight);
 				}
-				if (j > 0 && (j) <= 99 && field[i][j].getPassable()) {
+				if (j > 0 && (j) <= 99 && field[i][j].getResourceType() != 2) {
 					addLane("edge_" + i + j, arrayAt[i][j], arrayAt[i][j - 1],
 							weight);
 				}
 
-				if (i >= 0 && i < 99 && field[i][j].getPassable()) {
+				if (i >= 0 && i < 99 && field[i][j].getResourceType() != 2) {
 					addLane("edge_" + i + j, arrayAt[i][j], arrayAt[i + 1][j],
 							weight);
 				}
-				if ((j) >= 0 && j < 99 && field[i][j].getPassable()) {
+				if ((j) >= 0 && j < 99 && field[i][j].getResourceType() != 2) {
 					addLane("edge_" + i + j, arrayAt[i][j], arrayAt[i][j + 1],
 							weight);
 				}
@@ -148,7 +185,7 @@ public class ShortestPath {
 	}*/
 	
 	public static void main(String[] args) {
-		ShortestPath path = new ShortestPath(5,5,20,1);
+		ShortestPath path = new ShortestPath(5,5,20,20);
 		//System.out.println("Starting....");
 		//path.getPath();
 		//System.out.println("Finished!");
@@ -156,3 +193,4 @@ public class ShortestPath {
 	}
 
 }
+
