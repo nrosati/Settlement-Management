d87ff0856257c2bb6bc1c3a7fea2825be06bdeb3
nrosati <nickrosati2@mac.com>
2015-05-05 19:17:24 -0700

    Deleted unused Files

--- src/model/ResourcesFood.java
+++
@@ -1,7 +0,0 @@
-package model;
-public class ResourcesFood extends Resources{
-	public ResourcesFood(String name, int locationX, int locationY, boolean dense/*, Image image*/){
-		super(name, locationX, locationY, dense);
-	}
-
-}
--- src/model/ResourcesWater.java
+++
@@ -1,6 +0,0 @@
-package model;
-public class ResourcesWater extends Resources{
-	public ResourcesWater(String name, int locationX, int locationY, boolean dense/*,Image image*/) {
-		super(name, locationX, locationY, dense);
-	}
-}
